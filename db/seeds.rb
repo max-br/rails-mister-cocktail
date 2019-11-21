1db7cc8f7024a6870ad8a3a72540cb766bd2d9b6
Maxime Bruguier <maxime@azel.co>
2019-11-22 02:44:52 +0800

    add card banner et features

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+serialized_ingredients = open(url).read
+
+ingredients = JSON.parse(serialized_ingredients)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+puts "Yeah I created #{Ingredient.count}!"
+
+Cocktail.destroy_all
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Long Island Iced Tea")
+Cocktail.create(name: "Bloody Mary")
+
+puts "Yeah I created #{Cocktail.count}!"
