d8d8491a8a3dbfbf478c762c830647914ea58995
Maxime Bruguier <maxime@azel.co>
2019-11-22 15:30:23 +0800

    added cloudinary config file

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'ddllhchge'
+  config.api_key = '429111623828623'
+  config.api_secret = 'rjeM_wsoUvJ78P9UBXYlTGkRL7Q'
+  config.secure = true
+  config.cdn_subdomain = true
+end
